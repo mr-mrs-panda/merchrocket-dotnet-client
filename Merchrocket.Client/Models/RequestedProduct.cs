3df4b807d685fdf6013036c9c08a620057bdbc41
Patrick Knäpel <patrick.knaepel@pandaliebe.de>
2024-07-22 15:58:09 +0200

    #1207829435286737 feat(RequestedProduct model): add CatalogVariantIds property
    Added a new property, CatalogVariantIds, to the RequestedProduct model for selective mockup calculation based on variant IDs.

--- Merchrocket.Client/Models/RequestedProduct.cs
+++ Merchrocket.Client/Models/RequestedProduct.cs
@@ -4,8 +4,17 @@ namespace Merchrocket.Client.Models;
 
 public class RequestedProduct
 {
+    /// <summary>
+    /// The id of the catalogProduct.
+    /// </summary>
     [JsonPropertyName("catalogProductId")]
     public string? CatalogProductId { get; set; }
+    
+    /// <summary>
+    /// If you do not want to calculate the mockups for all variants of the product, but only for certain variants, then enter the variant IDs of the desired variants here.
+    /// </summary>
+    [JsonPropertyName("catalogVariantIds")]
+    public List<string>? CatalogVariantIds { get; set; }
 
     [JsonPropertyName("placements")]
     public List<Placement>? Placements { get; set; }
