17edef2f24583583c1bebd57cba2faa0c00db2e2
Patrick Knäpel <patrick.knaepel@pandaliebe.de>
2025-03-24 11:47:35 +0100

    feat(PlacementTechniques): add engraving placement technique

--- Merchrocket.Client/Constants/PlacementTechniques.cs
+++ Merchrocket.Client/Constants/PlacementTechniques.cs
@@ -3,4 +3,5 @@ namespace Merchrocket.Client.Constants;
 public class PlacementTechniques
 {
     public const string DirectPrint = "direct_print";
+    public const string Engraving = "engraving";
 }
